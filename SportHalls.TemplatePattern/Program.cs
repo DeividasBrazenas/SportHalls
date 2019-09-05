6e725d2c3318ae4775147f60cd867281799ee7af
Deividas <deividas.brazenas@gmail.com>
2019-09-05 16:06:07 +0300

    Adding template pattern project

---
+++ SportHalls.TemplatePattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SportHalls.TemplatePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
