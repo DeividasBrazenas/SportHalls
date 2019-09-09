e09f32b5fc5ab420d09ccf8f6c6ed7148092d2e6
Deividas <deividas.brazenas@gmail.com>
2019-09-10 00:10:26 +0300

    Adding IRentable

---
+++ SportHalls.StrategyPattern/RentStrategy/IRentable.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SportHalls.StrategyPattern.RentStrategy
+{
+    public interface IRentable
+    {
+        double CalculateRentCost(DateTime startDateTime, DateTime endDateTime);
+    }
+}
---
+++ SportHalls.StrategyPattern/RentStrategy/InventorRent/Inventor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SportHalls.StrategyPattern.RentStrategy.InventorRent
+{
+    public class Inventor : IRentable
+    {
+        private readonly IRentStrategy _rentStrategy;
+
+        public Inventor(IRentStrategy rentStrategy)
+        {
+            _rentStrategy = rentStrategy;
+        }
+
+        public double CalculateRentCost(DateTime startDateTime, DateTime endDateTime)
+        {
+            return _rentStrategy.CalculateRentCost(startDateTime, endDateTime);
+        }
+    }
+}
